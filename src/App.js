dd8035099f3eb99469fef7bcb7f2b575ee678b52
EmekaAgara <emekaagara@gmail.com>
2022-06-12 04:41:48 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 // import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token)
+    }
+
+    console.log("i have token", token);
+  },  []);
+
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <h1>I AM Logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
