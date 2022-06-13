d379d867ad91c78d7140f261116762c6099cf4e3
EmekaAgara <emekaagara@gmail.com>
2022-06-13 18:24:18 +0100

    login

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import './Login.css';
-import { loginUrl } from './spotify';
-
+import React from "react";
+import "./Login.css";
+import { accessUrl } from "./spotify";
 
 function Login() {
   return (
-    <div className='login'>
-        <img src='https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png' alt='' />
-        <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
-    </div>
-  )
+        <div className='login'>
+            <img src='https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png' alt='' />
+            <span>To continue login to Spotify</span>
+            <a href={accessUrl}>LOGIN WITH FACEBOOK</a>
+            <a href={accessUrl}>LOGIN WITH APPLE</a>
+            <a href={accessUrl}>LOGIN WITH GOOGLE</a>
+            <a href={accessUrl}>LOGIN WITH PHONENUMBER</a>
+            <a href={accessUrl}>LOGIN WITH SPOTIFY</a>
+        </div>
+    );
 }
+export default Login;
 
-export default Login
+
